c0c0642af4a0b653ad2ba8b61fbf3cb0d299a703
Cabbagelol <nickmiku@foxmail.com>
2025-07-21 09:00:37 +0800

    0.0.1

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import ships_zh_CN from './data/zh_CN/ships.json';
+import items_zh_CN from './data/zh_CN/item.json';
+import ships_en_US from './data/en_US/ships.json';
+import items_en_US from './data/en_US/item.json';
+
+export const zh_CN = {
+    ships: ships_zh_CN,
+    item: items_zh_CN
+};
+
+export const en_US = {
+    ships: ships_en_US,
+    item: items_en_US
+};
+
+// 导出类型
+export * from './types';
